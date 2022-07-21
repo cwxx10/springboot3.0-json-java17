19dcb392819a176d9d01e1ac4657fdb7f1e9d404
Ciro Carvalho <cmatheuzz@gmail.com>
2022-07-21 09:39:50 -0300

    second

---
+++ src/main/java/br/com/cwxx10/Greeting.java
@@ -0,0 +1,23 @@
+package br.com.cwxx10;
+
+public class Greeting {
+	//final não permite ter sets
+	private final long id;
+	private final String content;
+	
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+	
+}
---
+++ src/main/java/br/com/cwxx10/StartUp.java
@@ -0,0 +1,13 @@
+package br.com.cwxx10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
